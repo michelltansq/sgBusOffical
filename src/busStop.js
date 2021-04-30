5cc206bab7fd96cfcbd6331528bd90ac1dafef3e
Ju Long <19013345@myrp.edu.sg>
2021-04-30 22:02:04 +0800

    fail to complete favourite

--- src/busStop.js
+++ src/busStop.js
@@ -1,21 +1,35 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, Image, StyleSheet} from 'react-native';
+import {View, Text, FlatList, Image, StyleSheet, TouchableHighlight} from 'react-native';
 import MapView , { Marker }from 'react-native-maps';
 import { Ionicons } from '@expo/vector-icons';
-import { TouchableHighlight } from 'react-native-gesture-handler';
-import favData from './favData';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const busStop = ({navigation, route}) => {
  
+  const [favList, setFavList] = useState([]);
   const [busStopData, setBusStopData] = useState([]);
 
   useEffect(() => {
     (async () => {
-      let data = await fetch(`https://babasama.com/get_busstop_data?code=${route.params.code}&AccountKey=${route.params.AccountKey}`)
+      await fetch(`https://babasama.com/get_busstop_data?code=${route.params.code}&AccountKey=${route.params.AccountKey}`)
         .then((response) => response.json())
         .then((responseJson) => {
           setBusStopData(responseJson);
       }); 
+
+      try {
+        let data = [];
+        for (let i = 1; i > 0; i++) {
+          let jsonValue = await AsyncStorage.getItem(`${i}`);
+          if (jsonValue !== null) {
+            data.push(JSON.parse(jsonValue));
+          } else {
+            if (data.length > 0) {
+              setFavList(data);
+            }
+          }
+        }
+      } catch (e) { console.error(e); }
     })();
   }, []);
 
@@ -32,90 +46,161 @@ const busStop = ({navigation, route}) => {
           longitude: route.params.long
         }} title={"bus stop"}/>
       </MapView>
-      <FlatList data={busStopData} renderItem={({item}) => (
-        <View style={styles.flex}>
-          <Text style={styles.number}>{item.BusNumber}</Text>
-          <FlatList style={styles.data} data={item.BusData} renderItem={({item, index}) => {
-            if (item.BusModel === "DD") {
-              switch (index) {
-                case 0: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 0, 0, 0.5)'}]}>
-                        <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                        <Image source={require('../assets/doubledeck.png')} style={styles.busIcon}/>
-                      </View>
-                case 1: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 255, 0, 0.5)'}]}>
-                          <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                          <Image source={require('../assets/doubledeck.png')} style={styles.busIcon}/>
-                        </View>
-                case 2: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(0, 255, 0, 0.5)'}]}>
-                          <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                          <Image source={require('../assets/doubledeck.png')} style={styles.busIcon}/>
-                        </View>
-              }
-            } else if (item.BusModel === "SD") {
-              switch (index) {
-                case 0: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 0, 0, 0.5)'}]}>
-                        <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                        <Image source={require('../assets/singledeck.png')} style={styles.busIcon}/>
-                      </View>
-                case 1: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 255, 0, 0.5)'}]}>
-                        <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                        <Image source={require('../assets/singledeck.png')} style={styles.busIcon}/>
-                      </View>
-                case 2: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(0, 255, 0, 0.5)'}]}>
-                        <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                        <Image source={require('../assets/singledeck.png')} style={styles.busIcon}/>
-                      </View>
-              }
-            } else if (item.BusModel === "AB") {
-              switch (index) {
-                case 0: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 0, 0, 0.5)'}]}>
-                        <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                        <Image source={require('../assets/bendydeck.png')} style={styles.busIcon}/>
-                      </View>
-                case 1: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 255, 0, 0.5)'}]}>
-                        <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                        <Image source={require('../assets/bendydeck.png')} style={styles.busIcon}/>
-                      </View>
-                case 2: 
-                  return <View style={[styles.arrival, {backgroundColor: 'rgba(0, 255, 0, 0.5)'}]}>
-                        <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
-                        <Image source={require('../assets/bendydeck.png')} style={styles.busIcon}/>
-                      </View>
-              }
-            }
-          }}/>
-          <TouchableHighlight style={{marginRight: 90, width: 24}} onPress={() => {
-            let newData = {BusNumber: item.BusNumber, BusStopCode: route.params.code};
-            favData.push(newData);
-            print(favData);
-          }}>
-            <Ionicons style={styles.icon} name={'heart-outline'} size={24} />
-          </TouchableHighlight>
-        </View>
-      )}/>
+      <FlatList style={{width: '100%'}} data={busStopData} renderItem={({item}) => {
+        return busDisplay(favList, item, route);
+      }}/>
     </View>
   );
 };
 
+const busDisplay = (favList, item, route) => {
+  if (favList.length > 0) {
+    favList.forEach(i => {
+      if (i.BusStopCode === route.params.code && i.BusNumber === item.BusNumber) {
+        console.log(i);
+        return (
+          <View style={styles.flex}>
+            <Text style={styles.number}>{item.BusNumber}</Text>
+            <FlatList style={styles.data} data={item.BusData} renderItem={({item, index}) => {
+              return busModel(item, index);
+            }}/>
+            <TouchableHighlight style={{width: '100%',}} onPress={async() => {
+              const newValues = {
+                BusStopCode: route.params.code,
+                BusNumber: item.BusNumber
+              }
+              try {
+                for (let i = 0; i > -1; i++) {
+                  let jsonValue = await AsyncStorage.getItem(`${i}`);
+                  if (jsonValue === null) {
+                    await AsyncStorage.setItem(`${i}`, JSON.stringify(newValues));
+                    break;
+                  }
+                }
+              } catch (e) { console.error(e) }
+            }}>
+              <Ionicons style={styles.icon} name={'heart'} size={24} />
+            </TouchableHighlight>
+          </View>)
+          } else {
+        return (<View style={styles.flex}>
+          <Text style={styles.number}>{item.BusNumber}</Text>
+          <FlatList style={styles.data} data={item.BusData} renderItem={({item, index}) => {
+            return busModel(item, index);
+          }}/>
+          <TouchableHighlight style={{width: '100%',}} onPress={async() => {
+            const newValues = {
+              BusStopCode: route.params.code,
+              BusNumber: item.BusNumber
+            }
+            try {
+              for (let i = 0; i > -1; i++) {
+                let jsonValue = await AsyncStorage.getItem(`${i}`);
+                if (jsonValue === null) {
+                  await AsyncStorage.setItem(`${i}`, JSON.stringify(newValues));
+                  break;
+                }
+              }
+            } catch (e) { console.error(e) }
+          }}>
+            <Ionicons style={styles.icon} name={'heart-outline'} size={24} />
+          </TouchableHighlight>
+        </View>)
+      }
+    });
+  } else {
+    return (<View style={styles.flex}>
+          <Text style={styles.number}>{item.BusNumber}</Text>
+          <FlatList style={styles.data} data={item.BusData} renderItem={({item, index}) => {
+            return busModel(item, index);
+          }}/>
+          <TouchableHighlight style={{width: '100%',}} onPress={async() => {
+            const newValues = {
+              BusStopCode: route.params.code,
+              BusNumber: item.BusNumber
+            }
+            try {
+              for (let i = 0; i > -1; i++) {
+                let jsonValue = await AsyncStorage.getItem(`${i}`);
+                if (jsonValue === null) {
+                  await AsyncStorage.setItem(`${i}`, JSON.stringify(newValues));
+                  break;
+                }
+              }
+            } catch (e) { console.error(e) }
+          }}>
+            <Ionicons style={styles.icon} name={'heart-outline'} size={24} />
+          </TouchableHighlight>
+        </View>)}
+}
+
+const busModel = (item, index) => {
+  if (item.BusModel === "DD") {
+    switch (index) {
+      case 0: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 0, 0, 0.5)'}]}>
+              <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+              <Image source={require('../assets/doubledeck.png')} style={styles.busIcon}/>
+            </View>
+      case 1: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 255, 0, 0.5)'}]}>
+                <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+                <Image source={require('../assets/doubledeck.png')} style={styles.busIcon}/>
+              </View>
+      case 2: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(0, 255, 0, 0.5)'}]}>
+                <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+                <Image source={require('../assets/doubledeck.png')} style={styles.busIcon}/>
+              </View>
+    }
+  } else if (item.BusModel === "SD") {
+    switch (index) {
+      case 0: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 0, 0, 0.5)'}]}>
+              <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+              <Image source={require('../assets/singledeck.png')} style={styles.busIcon}/>
+            </View>
+      case 1: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 255, 0, 0.5)'}]}>
+              <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+              <Image source={require('../assets/singledeck.png')} style={styles.busIcon}/>
+            </View>
+      case 2: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(0, 255, 0, 0.5)'}]}>
+              <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+              <Image source={require('../assets/singledeck.png')} style={styles.busIcon}/>
+            </View>
+    }
+  } else if (item.BusModel === "AB") {
+    switch (index) {
+      case 0: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 0, 0, 0.5)'}]}>
+              <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+              <Image source={require('../assets/bendydeck.png')} style={styles.busIcon}/>
+            </View>
+      case 1: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(255, 255, 0, 0.5)'}]}>
+              <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+              <Image source={require('../assets/bendydeck.png')} style={styles.busIcon}/>
+            </View>
+      case 2: 
+        return <View style={[styles.arrival, {backgroundColor: 'rgba(0, 255, 0, 0.5)'}]}>
+              <Text style={{fontSize: 18, position: 'absolute', top: 2}}>{item.ArrivalTime} </Text>
+              <Image source={require('../assets/bendydeck.png')} style={styles.busIcon}/>
+            </View>
+    }
+  }
+}
+
 const styles = StyleSheet.create({
   flex: {
     width: '100%',
-    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingRight: 16,
   },
   number: {
-    width: 100,
+    width: '20%',
     fontWeight: 'bold',
     fontSize: 24,
     paddingLeft: 16,
@@ -127,16 +212,15 @@ const styles = StyleSheet.create({
     height: 60,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 10
+    borderRadius: 10,
   },
   icon: {
     justifyContent: 'center',
   },
-  data: {
-    flex: 1.3,
+  data: { 
+    width: '70%',
     flexDirection: 'row', 
     justifyContent: 'space-around',
-    marginRight: 30,
   },
   busIcon: {
     width: 50,
--- src/favourite.js
+++ src/favourite.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import {View, Text} from "react-native";
-import favData from './favData';
+import React, {useState, useEffect} from 'react';
+import {View, Text, FlatList} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function Favourite({ navigation }) {
+const Favourite = ({ navigation }) => {
+  const [favData, setFavData] = useState(null);
+  const [message, setMessage] = useState(<Text>No data is being stored</Text>);
+
+  const setData = async() => {
+    let data = [];
+    try {
+      await AsyncStorage.clear();
+      // for (let i = 1; i > 0; i++) {
+      //   let jsonValue = await AsyncStorage.getItem(`${i}`);
+      //   if (jsonValue !== null) {
+      //     data.push(JSON.parse(jsonValue));
+      //   } else {
+      //     break;
+      //   }
+      // }
+      // if (data.length !== 0) {
+      //   setFavData(data);
+      //   setMessage(<></>);
+      //   console.log(data);
+      // }
+    } catch (e) { console.error(e) }
+  }
+  
+  useEffect(() => {
+    setData();
+  })
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Favourite</Text>
+      {message}
+      <FlatList data={favData} renderItem={({item, index}) => (
+        <View>
+          <Text>{item.BusStopCode}</Text>
+        </View>
+      )}/>
     </View>
   );
 }
